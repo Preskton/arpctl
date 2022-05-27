26f5b372f7338f0890ec13aef1e640a0b9e006c4
Preston Doster <preston.doster@genoq.com>
2022-05-26 19:41:04 -0500

    gpio sniff pt 1

--- Makefile
+++ Makefile
@@ -2,10 +2,20 @@
 
 .PHONY: build
 build:
-	go build ./...
+	go build -o dist/bin/arpctl
 
 .PHONY: dist
 dist:
 	GOOS=linux GOARCH=amd64 go build -o dist/bin/arpctl-linux-amd64
 	GOOS=linux GOARCH=arm64 go build -o dist/bin/arpctl-linux-arm64
 	GOOS=linux GOARCH=arm go build -o dist/bin/arpctl-linux-arm
+
+.PHONY: run
+run:
+	@go run .
+
+ship: dist
+	scp dist/bin/arpctl-linux-arm preston@marceline:/home/preston/arpctl
+
+run-remote:
+	ssh preston@marceline '/home/preston/arpctl'
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	host "periph.io/x/host/v3"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var banner = `
+                                         █████    ████ 
+                                        ░░███    ░░███ 
+  ██████   ████████  ████████   ██████  ███████   ░███ 
+ ░░░░░███ ░░███░░███░░███░░███ ███░░███░░░███░    ░███ 
+  ███████  ░███ ░░░  ░███ ░███░███ ░░░   ░███     ░███ 
+ ███░░███  ░███      ░███ ░███░███  ███  ░███ ███ ░███ 
+░░████████ █████     ░███████ ░░██████   ░░█████  █████
+ ░░░░░░░░ ░░░░░      ░███░░░   ░░░░░░     ░░░░░  ░░░░░ 
+                     ░███                              
+                     █████                             
+                    ░░░░░                           
+`
 
 func main() {
-	fmt.Printf("yo")
+	fmt.Print(banner)
+	fmt.Print("\n")
+
+	log.Info("Initializing host...")
+	host.Init()
+
+	log.Info("Listing all GPIO pins")
+
+	printGpio(false, false)
+}
+
+func printGpio(invalid, showFunctions bool) {
+	all := gpioreg.All()
+	for _, p := range all {
+		print(p.String() + "\n")
+	}
 }
